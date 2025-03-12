848848b5523ae400a252a0e4cc25dc7c29aca236
Stilian Solinkov <SPSolnikov23@codingburgas.bg>
2025-03-12 21:35:16 +0200

    Add menu and fix banner

--- main.cpp
+++ main.cpp
@@ -14,5 +14,37 @@ int main() {
     string banner7 = " \_______) |__|  \___)(___/    \___)(________/  \_______)(_______/  \'_____/    \'_____/   (__|  \__) ";
     string banner8 = "                                                                                                      ";
 
+    cout << banner1 << endl << banner2 << endl << banner3 << endl << banner4 << endl << banner5 << banner6 << endl << banner7 << endl << banner8;
+
+
+    cout << "Enter your choice!" << endl;
+    cout << "1.Do a test" << endl;
+    cout << "2.Grade scale" << endl;
+    cout << "3.Exit" << endl;
+
+
+    int choise;
+    cin >> choise;
+
+    switch(choise){
+        case 1:
+            
+
+        break;
+
+        case 2:
+                //view all students
+        break;
+
+        
+        case 3:
+            return 0;
+        break;
+
+        default:
+        cout << "Invalid option!";
+        break;
+    }
+
 
 }
