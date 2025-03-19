513d18fa44bd36bf8b973e73002362a615c2d5d8
GVLitsanowski23 <GVLitsanowski23@codingburgas.bg>
2025-03-19 22:56:17 +0200

    Update submodule changes

--- main.cpp
+++ main.cpp
@@ -218,20 +218,19 @@ vector<Question>spanishExam() {
 
 }
 
-
+void displayBanner() 
+{
+    cout << "   ___ _          _   __ _                       " << endl;
+    cout << "  / _ (_)_  _____| | / _\\ |_ ___  _ __ _ __ ___  " << endl;
+    cout << " / /_)/ \\ \\/ / _ \\ | \\ \\| __/ _ \\| '__| '_ ` _ \\ " << endl;
+    cout << "/ ___/| |>  <  __/ | _\\ \\ || (_) | |  | | | | | |" << endl;
+    cout << "\\/    |_/_/\\_\\___|_| \\__/\\__\\___/|_|  |_| |_| |_|" << endl;
+}
 
 int main() {
+    displayBanner();
 
-    string banner1 = "  _______    _______        __       ________    _______  _______     ______      ______    __   ___  ";
-    string banner2 = " /'' _   |  /'      \      /''\     |'      '\  /'     '||   _  '\   /    ' \    /    ' \  |/'| /  ') ";
-    string banner3 = "(: ( \___) |:        |    /    \    (.  ___  :)(: ______)(. |_)  :) // ____  \  // ____  \ (: |/   /  ";
-    string banner4 = " \/ \      |_____/   )   /' /\  \   |: \   ) || \/    |  |:     \/ /  /    ) :)/  /    ) :)|    __/   ";
-    string banner5 = " //  \ ___  //      /   //  __'  \  (| (___\ || // ___)_ (|  _  \\(: (____/ //(: (____/ // (// _  \   ";
-    string banner6 = "(:   _(  _||:  __   \  /   /  \\  \ |:       :)(:      '||| |_)  :)\        /  \        /  |: | \  \  ";
-    string banner7 = " \_______) |__|  \___)(___/    \___)(________/  \_______)(_______/  \'_____/    \'_____/   (__|  \__) ";
-    string banner8 = "                                                                                                      ";
-
-    cout << endl << banner1 << endl << banner2 << endl << banner3 << endl << banner4 << endl << banner5 << banner6 << endl << banner7 << endl << banner8;
+  
 
 
     cout << "Enter your choice!" << endl;
