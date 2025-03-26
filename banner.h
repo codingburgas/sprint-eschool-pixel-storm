b7bb7d16e24e2539d741386994b68b9a6b5234fc
GVLitsanowski23 <GVLitsanowski23@codingburgas.bg>
2025-03-26 23:21:17 +0200

    Banner design updated with color

--- banner.h
+++ banner.h
@@ -2,6 +2,16 @@
 #ifndef BANNER_H
 #define BANNER_H
 
-void displayBanner();
+void displayBanner()
+{
+    std::cout << "\033[1;32m"; //  Green Colour;
+    std::cout << "   ___ _          _   __ _                       " << std::endl;
+    std::cout << "  / _ (_)_  _____| | / _\\ |_ ___  _ __ _ __ ___  " << std::endl;
+    std::cout << " / /_)/ \\ \\/ / _ \\ | \\ \\| __/ _ \\| '__| '_ ` _ \\ " << std::endl;
+    std::cout << "/ ___/| |>  <  __/ | _\\ \\ || (_) | |  | | | | | |" << std::endl;
+    std::cout << "\\/    |_/_/\\_\\___|_| \\__/\\__\\___/|_|  |_| |_| |_|" << std::endl;
+    std::cout << "\033[0m"; // Reset the colour
+}
+
 
 #endif // BANNER_H
